6cdda26ff91faabed41816f957584c84c55cd104
kainat <kainatsid93@gmail.com>
2018-01-11 10:02:44 +0500

    NewUser132

--- app/src/main/java/com/example/kainat/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/kainat/myfirstapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
        setContentView(R.layout.activity_main);
-       Button btn= (Button) findViewById(R.id.button);
+       Button btna= (Button) findViewById(R.id.button);
        TextView txt=(TextView) findViewById(R.id.txt);
     }
 }
